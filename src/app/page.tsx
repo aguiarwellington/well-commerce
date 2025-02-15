b479db3dac50b1bc642f3e4bb66ae916bb2aa395
aguiarwellington <wellingtoncarvalho908@gmail.com>
2025-02-15 17:26:30 -0300

    Listagem de elementos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 
 export default function Home() {
   return (
-   <div>
+   <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+    <div className="grid grid-cols1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+
+
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      prod 1
+    </div>
+
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      prod 2
+    </div>
+
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      prod 3
+    </div>
+
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      prod 4
+    </div>
+
+    </div>
+
+ 
 
    </div>
   );
